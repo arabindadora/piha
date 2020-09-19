14d872a8afad5a1c56a1a3c6ffad9dce028b314f
Arabinda Dora <devlife404@gmail.com>
2020-09-19 23:49:54 +0530

    feat: setup, doc and first full implementation :sparkles:

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # piha
+
+A twitter bot that automatically tweets a rant.
+
+## What's with the name?
+Named after the screaming bird [piha](https://en.wikipedia.org/wiki/Screaming_piha), it represents the bot's action - ranting/screaming. 😜
+
+## Why is it built?
+1. I wanted to practice my recently acquired Golang skills.
+2. My internet service provider was extremely terrible at addressing requests. Tired of following up with them, I wrote this bot to do that for me repeatedly and automatically until my issues are resolved.
+3. We all need a twitter bot at some point in our life! 😉
+
+## How do I set it up?
+1. You will need a twitter developer account. Follow the instructions [here](https://developer.twitter.com/en/portal) to set up a new twitter bot app and gather the following secrets:
+    * API_KEY
+    * API_KEY_SECRET
+    * ACCESS_TOKEN
+    * ACCESS_TOKEN_SECRET
+2. run `cp .env.example .env`
+3. edit `.env` file and set the secrets gathered from step #1
+4. edit `.env` file and setup the below configurations
+    * RANT_USER - the twitter handle of the service provider
+    * RANT_DATE - the date in YYYY-MM-DD format when your issue with the service provider occurred
+    * RANT_TEMPLATE - the rant tweet template. DO NOT change the text inside `{{}}` including the braces.
+
+## How do I run it?
+1. run `go mod download`
+2. run `go run piha.go`
+
+## TODO
+1. Document repeated invocation via a Cron job or its alternative
+2. Explore building binaries
+3. Explore GitHub workflow integration
---
+++ piha.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"text/template"
+	"time"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+)
+
+// Credentials stores the secret keys needed
+// for authentication against the Twitter API
+type Credentials struct {
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessToken       string
+	AccessTokenSecret string
+}
+
+// NewCredentials creates a new Credentials
+// struct reading the secrets from .env file
+func NewCredentials() Credentials {
+	return Credentials{
+		ConsumerKey:       os.Getenv("API_KEY"),
+		ConsumerSecret:    os.Getenv("API_KEY_SECRET"),
+		AccessToken:       os.Getenv("ACCESS_TOKEN"),
+		AccessTokenSecret: os.Getenv("ACCESS_TOKEN_SECRET"),
+	}
+}
+
+// NewTwitterClient takes a Credentials struct and
+// returns a twitter client
+func NewTwitterClient(creds *Credentials) *twitter.Client {
+	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+	return twitter.NewClient(httpClient)
+}
+
+// RantConfig stores the rant configuration
+type RantConfig struct {
+	User     string
+	Days     string
+	Suffix   string
+	Template string
+}
+
+// NewRantConfig creates a new rant config struct
+// reading the values from .env file
+func NewRantConfig() RantConfig {
+	return RantConfig{
+		User:     os.Getenv("RANT_USER"),
+		Days:     getRantDays(os.Getenv("RANT_DATE")),
+		Suffix:   getRandomRantSuffix(),
+		Template: os.Getenv("RANT_TEMPLATE"),
+	}
+}
+
+// getRantDays computes the number of days
+// since a given date
+func getRantDays(date string) string {
+	layout := "2006-01-02"
+	since, err := time.Parse(layout, date)
+	if err != nil {
+		// fallback
+		return "SEVERAL"
+	}
+	// count the number of days elapsed
+	days := int(time.Now().Sub(since).Hours() / 24)
+	return strconv.Itoa(days)
+}
+
+// getRandomRantSuffix returns a random rant
+// suffix from a predfined list
+func getRandomRantSuffix() string {
+	// list of rant suffixes
+	rants := []string{
+		"Where is my money? 😡",
+		"Are you proud of this? 😒",
+		"Do you do this to all your customers? 🙄",
+	}
+	rand.Seed(time.Now().UnixNano())
+	// pick a random rant from the list
+	return rants[rand.Intn(len(rants))]
+}
+
+// NewRant creates a new rant from the template
+// with a random rant suffix
+func NewRant() (string, error) {
+	config := NewRantConfig()
+	tmpl, err := template.New("rant").Parse(config.Template)
+	if err != nil {
+		return "", err
+	}
+	// capture the template output to a var
+	var rant bytes.Buffer
+	err = tmpl.Execute(&rant, config)
+	if err != nil {
+		return "", err
+	}
+	return rant.String(), nil
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	rant, err := NewRant()
+	if err != nil {
+		// no rant, no fun. exit.
+		log.Fatal("Couldn't make a new rant")
+	}
+
+	// setup credentials and twitter client
+	creds := NewCredentials()
+	client := NewTwitterClient(&creds)
+
+	// post the rant
+	_, _, err = client.Statuses.Update(rant, nil)
+	if err != nil {
+		log.Fatal("Rant didn't go through!")
+	}
+	log.Print("Tweeted rant: ", rant)
+}
